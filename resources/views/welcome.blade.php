bcf90819a3b3f81e5658fd4905a549028df1bd34
Aakash Desai <aakashdesai.pro@gmail.com>
2023-02-18 21:51:56 +0530

    vue setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
-        <h1>HomePAge</h1>
+        <div id="app"></div>
+        @vite('resources/js/app.js')
     </body>
 </html>
